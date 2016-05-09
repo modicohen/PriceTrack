b9c5e844d7f134c0c48cf5edaf68e6ae2b3b54f0
David Cohen <David Cohen@DAVIDCOHENPC.home>
2016-05-09 17:19:41 -0400

    Added comments to main

--- PriceTrack/src/Main.java
+++ PriceTrack/src/Main.java
@@ -13,16 +13,19 @@ public class Main extends Application{
 	public static void main(String[] args) {
 		launch(args);
 	}
-
+		//The button and text field
 		@Override public void start(Stage primaryStage) throws IOException    {
 		btn=new Button();
 		TextField search=new TextField();
+		//Setting font to Verdana 20 for button and text
 		btn.setFont(Font.font("Verdana",20));
 		btn.setText("Search!");
 		search.setFont(Font.font("Verdana",20));
+		//Adding text field and button to GridPane
 		GridPane pane = new GridPane();
 		pane.add(search,0,1);
 		pane.add(btn,2,1); 
+		//Adding pane to scene, scene to stage
 		Scene scene=new Scene(pane);
 		primaryStage.setScene(scene);        
 		primaryStage.setTitle("Price Tracker");        
